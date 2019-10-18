46239e1a130ae43b887b682c98af46735c8057c0
VARGA <silentwolfdev@outlook.com>
2019-10-19 01:17:05 +0200

    ToggleButton animation working

---
+++ KIWI/Converters/MultiMultiplierConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace KIWI.Converters
+{
+    /// <summary>
+    /// Defines a converter which multiplies all provided values.
+    /// The given parameter indicates number of arguments to multiply.
+    /// </summary>
+    public class MultiMultiplierConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double result = 1;
+            int count = int.Parse((string)parameter);
+            for (int i = 0; i < count; i++)
+            {
+                result *= (double)values[i];
+            }
+            return result;
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+}
---
+++ KIWI/Converters/OppositeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace KIWI.Converters
+{
+    /// <summary>
+    /// Defines a converter which multiplies the provided value by -1
+    /// </summary>
+    public class OppositeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (dynamic)value * -1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+}
