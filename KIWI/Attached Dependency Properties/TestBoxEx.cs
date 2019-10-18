fdf56c6970d2dc93b8e0cc1f3bc120ec0758531a
VARGA <silentwolfdev@outlook.com>
2019-10-18 06:32:42 +0200

    Uploading files

---
+++ KIWI/Attached Dependency Properties/TestBoxEx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KIWI.Attached_Dependency_Properties
+{
+    public static class ButtonHelper
+    {
+        public static readonly DependencyProperty
+            SecondaryColorProperty =
+                DependencyProperty.RegisterAttached(
+                    "SecondaryColor", typeof(string), typeof(ButtonHelper),
+                    new PropertyMetadata("")); 
+        public static string GetSecondaryColor(
+            DependencyObject d)
+        {
+            return (string)d.GetValue(SecondaryColorProperty);
+        }
+        public static void SetSecondaryColor(
+            DependencyObject d, string value)
+        {
+            d.SetValue(SecondaryColorProperty, value);
+        }
+    }
+}
---
+++ KIWI/Converters/ToggleButtonMultiValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace KIWI.Converters
+{
+    class ToggleButtonMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var thumb = (double)values[0];
+            var handle = (double)values[1];
+
+            return handle - thumb;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
